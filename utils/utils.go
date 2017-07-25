9a09287cf10a87d7571da8ba9b06fc428d77df37
Sylvain Friquet <sylvain.friquet@algolia.com>
2017-07-25 17:44:58 +0200

    remove dead code

--- utils/utils.go
+++ utils/utils.go
@@ -1,13 +1,10 @@
 package utils
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/csv"
 	"encoding/json"
-	"fmt"
 	"os"
-	"os/exec"
 )
 
 func WriteJSON(file *os.File, payload interface{}) error {
@@ -52,54 +49,6 @@ func AppendToFile(filename string, line string) error {
 	defer f.Close()
 
 	if _, err := f.WriteString(line); err != nil {
-		return err
-	}
-	return nil
-}
-
-func ExecCommand(cmd *exec.Cmd) error {
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return err
-	}
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		return err
-	}
-
-	go func() {
-		scanner := bufio.NewScanner(stdout)
-		for scanner.Scan() {
-			fmt.Println(scanner.Text())
-		}
-	}()
-
-	go func() {
-		scanner := bufio.NewScanner(stderr)
-		for scanner.Scan() {
-			fmt.Println(scanner.Text())
-		}
-	}()
-
-	err = cmd.Start()
-	if err != nil {
-		return err
-	}
-
-	err = cmd.Wait()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func EnsureEmptyDir(path string) error {
-	err := os.RemoveAll(path)
-	if err != nil {
-		return err
-	}
-	err = os.MkdirAll(path, os.ModePerm)
-	if err != nil {
 		return err
 	}
 	return nil
